b9d7f96a51a479b373f2eb3d0b1206ecc51793cc
liaojian <liaojianzls@163.com>
2017-02-13 11:51:42 +0800

    添加demo示例

--- UIView-Frame/ViewController.m
+++ UIView-Frame/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "UIView+Frame.h"
 @interface ViewController ()
 
 @end
@@ -16,7 +16,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    UIView *view1 = [UIView new];
+    UIView *view2 = [UIView new];
+    UIView *view3 = [UIView new];
+    UIView *view4 = [UIView new];
+    
+    view1.backgroundColor = [UIColor blackColor];
+    view2.backgroundColor = [UIColor greenColor];
+    view3.backgroundColor = [UIColor blueColor];
+    view4.backgroundColor = [UIColor orangeColor];
+    
+    view1.viewSize   = CGSizeMake(200, 200);
+    view1.viewOrigin = CGPointMake(100, 100);
+    
+    view2.viewSize   = CGSizeMake(180, 180);
+    view2.x          = 100;
+    view2.y          = 100;
+    
+    view3.viewSize   = CGSizeMake(160, 160);
+    view3.center     = CGPointMake(180, 180);
+    
+    view4.viewSize   = CGSizeMake(140, 140);
+    view4.top        = 100;
+    view4.left       = 100;
+    
+    [self.view addSubview:view1];
+    [self.view addSubview:view2];
+    [self.view addSubview:view3];
+    [self.view addSubview:view4];
 }
 
 
